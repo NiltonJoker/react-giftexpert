05db57234076a96449334bf6a58deb27de65f1b1
Nilton <nilton242009@hotmail.com>
2021-03-01 10:36:46 -0500

    First commit

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import AddCategory from "./AddCategory";
+import GifGrid from "./GifGrid";
+
+const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(["Dragon Ball Z"]);
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories}/>
+      <hr />
+
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category}/>
+        ))}
+      </ol>
+    </>
+  );
+};
+
+export default GifExpertApp;
